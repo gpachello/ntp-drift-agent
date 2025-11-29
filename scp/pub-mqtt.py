fd3b0dd2b4a7b7b5b32e30e93b44910480a90154
Gustavo Pachello <gpachello@users.noreply.github.com>
2025-11-29 14:07:44 -0300

    Delete scp directory

--- scp/pub-mqtt.py
+++
@@ -1,8 +0,0 @@
-import paho.mqtt.client as mqtt
-
-client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, 'id0001')
-
-client.connect("mqtt", 1883, 60)
-
-client.publish("TEST", "Mensaje de prueba")
-client.disconnect()
